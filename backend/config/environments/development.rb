9dfd59a07bd21ec1c7e93483c02180706e747bf0
eusantos <eusantos11@outlook.com>
2021-08-20 23:55:48 -0300

    Update development.rb

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   config.hosts << "smartdoor-server.tadsufpr.net.br"
-  config.hosts << "smartdoor.tadsufpr.net.br/"
+  config.hosts << "smartdoor.tadsufpr.net.br"
   
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
